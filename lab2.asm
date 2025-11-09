b15d7d351de19e0a186a1a7b1227ec3c6682e5ef
SoucaThomas <thomassouca@gmail.com>
2025-11-09 14:22:28 +0200

    initail commit

---
+++ lab2.asm
@@ -0,0 +1,244 @@
+%include 'mio.inc'
+
+section .data
+dec_prompt db "Adj meg egy elojeles decimalis szamot: ",0
+hex_prompt db "Adj meg egy pozitiv hexadecimalis szamot: ",0
+dec_out db "Decimalis alak: ",0
+hex_out db "Hexadecimalis alak: ",0
+sum_out db "Osszeg: ",0
+hiba_dec db "Hiba: Nem megengedett karakter! Kerem ujra.",0
+hiba_hex db "Hiba: Nem megengedett karakter! Kerem ujra.",0
+hexa_prefix db "0x",0
+
+section .bss
+num1 resd 1
+num2 resd 1
+sum  resd 1
+
+section .text
+global main
+
+main:
+    push esi
+    push ebx
+
+    mov esi,dec_prompt
+    call mio_writestr
+    call readDec
+    mov [num1],eax
+
+    mov esi,dec_out
+    call mio_writestr
+    mov eax,[num1]
+    call writeDec
+    call mio_writeln
+
+    mov esi,hex_out
+    call mio_writestr
+    mov eax,[num1]
+    call writeHex
+    call mio_writeln
+
+    mov esi,hex_prompt
+    call mio_writestr
+    call readHex
+    mov [num2],eax
+
+    mov esi,dec_out
+    call mio_writestr
+    mov eax,[num2]
+    call writeDec
+    call mio_writeln
+
+    mov esi,hex_out
+    call mio_writestr
+    mov eax,[num2]
+    call writeHex
+    call mio_writeln
+
+    mov eax,[num1]
+    add eax,[num2]
+    mov [sum],eax
+
+    mov esi,sum_out
+    call mio_writestr
+    mov eax,[sum]
+    call writeDec
+    call mio_writeln
+
+    mov esi,hex_out
+    call mio_writestr
+    mov eax,[sum]
+    call writeHex
+    call mio_writeln
+
+    pop ebx
+    pop esi
+    ret
+
+readDec:
+    xor eax,eax
+    mov esi,1
+    call mio_readchar
+    cmp al,'-'
+    jne .loop_start
+    mov esi,-1
+    call mio_readchar
+.loop_start:
+    cmp al,13
+    je .done_zero
+.loop:
+    cmp al,13
+    je .done
+    cmp al,'0'
+    jb .error_dec
+    cmp al,'9'
+    ja .error_dec
+    sub al,'0'           
+    mov ebx,eax          
+    shl eax,3
+    shl ebx,1
+    add eax,ebx           
+    add eax,edx           
+    mov edx,0
+    mov dl,al
+    call mio_readchar
+    jmp .loop
+.done:
+    imul eax,esi
+    ret
+.done_zero:
+    xor eax,eax
+    ret
+.error_dec:
+    mov esi,hiba_dec
+    call mio_writestr
+    call mio_writeln
+    jmp readDec
+
+writeDec:
+    push eax
+    push ebx
+    push ecx
+    push edx
+
+    cmp eax,0
+    jns .pos
+    mov al,'-'
+    call mio_writechar
+    neg eax
+
+.pos:
+    mov ecx,0
+    mov ebx,10
+.dec_loop:
+    xor edx,edx
+    div ebx
+    push edx
+    inc ecx
+    test eax,eax
+    jnz .dec_loop
+
+.print_loop:
+    cmp ecx,0
+    je .done_print
+    pop edx
+    add dl,'0'
+    mov al,dl
+    call mio_writechar
+    dec ecx
+    jmp .print_loop
+
+.done_print:
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+    ret
+
+
+readHex:
+    xor eax,eax
+.loop:
+    call mio_readchar
+    cmp al,13
+    je .done_hex
+
+    cmp al,'0'
+    jb .error_hex
+    cmp al,'9'
+    jbe .digit
+    cmp al,'A'
+    jb .lower
+    cmp al,'F'
+    jbe .upper
+    cmp al,'a'
+    jb .error_hex
+    cmp al,'f'
+    ja .error_hex
+
+.lower:
+    sub al,'a'
+    add al,10
+    jmp .add_value
+.upper:
+    sub al,'A'
+    add al,10
+    jmp .add_value
+.digit:
+    sub al,'0'
+
+.add_value:
+    shl eax,4          
+    mov edx,0
+    mov dl,al          
+    add eax,edx
+    jmp .loop
+
+.done_hex:
+    ret
+
+.error_hex:
+    mov esi,hiba_hex
+    call mio_writestr
+    call mio_writeln
+    jmp readHex
+
+
+
+writeHex:
+    push eax
+    push ebx
+    push ecx
+    push edx
+    push esi
+
+    mov esi, hexa_prefix
+    call mio_writestr
+
+    mov ecx, 8
+    mov ebx, eax
+
+.hex_loop:
+    mov eax, ebx
+    shr eax, 28         
+    mov edx, eax         
+    cmp dl, 10
+    jb .digit
+    add dl, 'A'-10
+    jmp .write_char
+.digit:
+    add dl, '0'
+.write_char:
+    mov al, dl
+    call mio_writechar
+    shl ebx, 4           
+    dec ecx
+    jnz .hex_loop
+
+    pop esi
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+    ret
